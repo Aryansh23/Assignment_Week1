0fbdcf02248d1d05219ef4289e8193655c7856d9
Aryansh23 <98678055+Aryansh23@users.noreply.github.com>
2022-02-11 23:40:13 +0530

    Add files via upload

---
+++ Question_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter 1st no.: \n");
+    scanf("%d",&a);
+    printf("Enter 2nd no.: \n");
+    scanf("%d",&b);
+    if(a>b)
+    {
+        printf("The greater no. is %d",a);
+    }
+    else
+    {
+        printf("The greater no. is %d",b);
+    }
+        return 0;
+
+}
